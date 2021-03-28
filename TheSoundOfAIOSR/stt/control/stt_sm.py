d243419e7f74bf8415d06eb55f04bce5f403800a
Tibor Kiss <kiss.tibor@gmail.com>
2021-03-28 21:25:37 +0200

    wsserver and state machine working together

--- TheSoundOfAIOSR/stt/control/stt_sm.py
+++ TheSoundOfAIOSR/stt/control/stt_sm.py
@@ -10,24 +10,32 @@ from TheSoundOfAIOSR.stt.control.model_loader import load_stt_models as load_mod
 class SpeechToTextSM(sm.StateMachine):
     class States(sm.StateEnum):
         uninitialized = "Uninitialized."
+        restarted = "Restarted."
         models_preloading = "Preloading the models..."
         models_ready = "The models are preloaded and ready."
         capture_and_transcribe = "Capture and transcribe..."
         idle = "Idle..."
 
-    microphone_name: Optional[str] = None
+    selected_microphone: Optional[str] = None
+    last_transcription: Optional[str] = None
+
 
     def select_microphone(self, name):
-        self.microphone_name = name
+        self.selected_microphone = name
+
+
+    def set_last_transcription(self, text):
+        self.last_transcription = text
 
 
     @sm.event(None, States.uninitialized)
     async def reset(self, logger: logging.Logger) -> None:
         logger.debug("uninitialized action")
-    
 
-    @sm.event(source=States.uninitialized, target=States.models_preloading)
-    async def load_stt_models(self, logger: logging.Logger) -> None:
+
+    @sm.event(source=[States.uninitialized, States.models_ready, States.idle],
+              target=States.models_preloading, return_type=bool)
+    async def load_stt_models(self, logger: logging.Logger):
         logger.debug("preload_models action schedules load stt models ...")
         # since it is a long running process, we execute in threadpool
         loop = asyncio.get_running_loop()
@@ -35,6 +43,7 @@ class SpeechToTextSM(sm.StateMachine):
         # it will be resumed only after the load is done
         await self.models_ready(logger)
         logger.debug("preload_models action return.")
+        return True
 
     
     @sm.event(source=States.models_preloading, target=States.models_ready)
@@ -42,11 +51,18 @@ class SpeechToTextSM(sm.StateMachine):
         logger.debug("models_ready.")
 
 
-    @sm.event(source=States.models_ready, target=States.capture_and_transcribe)
-    async def start_capture_and_transcribe(self, logger: logging.Logger) -> None:
+    @sm.event(source=States.models_ready,
+              target=States.capture_and_transcribe,
+              return_type=bool)
+    async def start_capture_and_transcribe(self, logger: logging.Logger):
         logger.debug("start_capture_and_transcribe action.")#, self.microphone_name)
+        return True
 
-    
-    @sm.event(source=States.capture_and_transcribe, target=States.idle)
-    async def stop_transcription(self, logger: logging.Logger) -> None:
+
+    @sm.event(source=States.capture_and_transcribe,
+              target=States.idle,
+              return_type=bool)
+    async def stop_transcription(self, logger: logging.Logger):
+        self.last_transcription = "Give me a guitar"
         logger.debug("stop_transcription")
+        return True
--- TheSoundOfAIOSR/stt/interface/wsserver.py
+++ TheSoundOfAIOSR/stt/interface/wsserver.py
@@ -12,7 +12,7 @@ class SimpleServerInterface(WebsocketServer):
         self._register(self.start)
         self._register(self.stop)
         self._register(self.fetch)
-        self._control = SpeechToTextSM()
+        self._control = SpeechToTextSM(state=SpeechToTextSM.States.uninitialized)
 
     """
     This overrides _consumer method in WebsocketServer, there should
@@ -25,26 +25,36 @@ class SimpleServerInterface(WebsocketServer):
             await websocket.send(gen)
 
     async def setup_model(self):
-        await self._control.reset(logger=logging)
-        await self._control.load_stt_models(logger=logging)
-        yield json.dumps({"resp": True})
+        try:
+            yield json.dumps({"resp":
+                await self._control.load_stt_models(logger=logging)})
+        except RuntimeError as re:
+            yield json.dumps({"reps": False, "error": "{0}".format(re)})
 
     async def status(self):
-        yield json.dumps({"resp": f"{self._control.state.name if self._control.state else 'None'}"})
+        yield json.dumps({"resp": f"{self._control.state.name}"
+                         if self._control.state else 'None'})
 
     async def select_microphone(self, name):
-        logging.debug("select microphone {}", name)
         self._control.select_microphone(name)
         yield json.dumps({"resp": name})
 
     async def start(self):
-        await self._control.start_capture_and_transcribe(logger=logging)
-        yield json.dumps({"resp": True})
+        try:
+            yield json.dumps({"resp":
+                await self._control.start_capture_and_transcribe(logger=logging)})
+        except RuntimeError as re:
+            yield json.dumps({"reps": False, "error": "{0}".format(re)})
 
     async def stop(self):
-        await self._control.stop_transcription(logger=logging)
-        yield json.dumps({"resp": True})
+        try:
+            yield json.dumps({"resp":
+                await self._control.stop_transcription(logger=logging)})
+        except RuntimeError as re:
+            yield json.dumps({"reps": False, "error": "{0}".format(re)})
 
     async def fetch(self):
         logging.debug("fetch")
-        yield json.dumps({"resp": "the transcribed test is this"})
+        self._control.last_transcription
+        yield json.dumps({"resp": f"{self._control.last_transcription}"
+                         if self._control.last_transcription else ''})
